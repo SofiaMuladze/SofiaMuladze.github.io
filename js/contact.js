281343ad0546a3905e4157021083f4f7b4228d4e
SofiaMuladze <So.Muladze@gmail.com>
2022-09-12 20:53:40 +0200

    second commit

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,7 @@
 (function () {
 emailjs.init("4AyC6t-ChVb2cRpZb");
 })();
-// listen to the form submission
+
 document
 .getElementById("myForm")
 .addEventListener("submit", function (event) {
@@ -11,7 +11,6 @@ document
     const serviceID = "service_5duzeii";
     const templateID = "template_e66ikzg";
 
-    // send the email here
     emailjs.sendForm(serviceID, templateID, this).then(
         (response) => {
             console.log("SUCCESS!", response.status, response.text);
